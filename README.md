0a64442db3f733233daaa3c89fb92181e7580d86
Hubert <hubert@singular.live>
2016-05-12 14:35:48 +0700

    cleanup of output.html and deploy.js

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 	node.js
 
 ## Project setup
-	1. Get your deploy key from Singular.Live team then put it in "singularwidget.json"
-	2. Put your widget code in "source" directory
-    3. Run "node deploy.js"
+	1. Run "npm install"
+	2. Get your deploy key from Singular.Live team then put it in "singularwidget.json"
+	3. Put your widget code in "source" directory
+    4. Run "node deploy.js"
+	
--- deploy.js
+++ deploy.js
@@ -1,10 +1,16 @@
-console.log('------------------------------------');
-console.log('---Start deploying widget to Singular.Live---');
+console.log('-----------------------------------------------');
+console.log('Singular.Live widget deploy - version 1.0');
 
-var config = require('./singularwidget.json');
-if (!config.deploykey) {
-  console.error('No deploy key provided in singularwidget.json');
-  return;
+try {
+	var config = require('./singularwidget.json');
+	if (!config.deploykey) {
+		console.error('ERROR: Cannot find deploy key in singularwidget.json');
+		return;
+	}
+}
+catch (e) {
+	console.error('ERROR: Cannot access singularwidget.json: ' + e);
+	return;
 }
 
 var fs = require('fs');
@@ -12,64 +18,57 @@ var zipdir = require('zip-dir');
 var xhr = require('superagent');
 var deployUrl = 'https://staging-enigmatic-ocean-3936.herokuapp.com/widgets/deploywithkey';
 
-console.log('---------------------');
-console.log('Validate files in "source"');
+console.log('Validating files in directory "source"');
 
 // Check for output.html and icon.png
 try {
   var stats = fs.lstatSync('./source');
 
   if (stats.isDirectory()) {
-
     try {
       var outputHtmlFile = fs.lstatSync('./source/output.html');
     } catch(e) {
-      console.error('Can\'t find "source/output.html" file');
+      console.error('ERROR: Cannot find file "source/output.html"');
       return;
     }
-
     try {
       var iconPngFile = fs.lstatSync('./source/icon.png');
     } catch(e) {
-      console.error('Can\'t find "source/icon.png" file');
+      console.error('ERROR: Cannot find file "source/icon.png"');
       return;
     }
-
   } else {
-    console.error('Can\'t find "source" directory');
+    console.error('ERROR: "source" is not a directory');
     return;
   }
-
 } catch(e) {
-  console.error('Can\'t find "source" directory');
+  console.error('ERROR: Cannot find directory "source"');
   return;
 }
 
 // Zip source folder
-console.log('Make widget package');
+console.log('Creating zip file');
 
-zipdir('./source', { saveTo: './widgetpack.zip' }, function (err, buffer) {
-  // `buffer` is the buffer of the zipped file
-  // And the buffer was saved to `~/myzip.zip`
+zipdir('./source', { saveTo: './SingularWidget.zip' }, function (err, buffer) {
   if (err) {
-    console.error('Can\'t make package from "source" directory');
+    console.error('ERROR: Cannot zip directory "source"');
     return;
   } else {
-    console.log('Deploy widget to Singular.Live');
+    console.log('Deploying widget to Singular.Live');
 
     // Upload source folder to Singular.Live
     var req = xhr.put(deployUrl);
     req.field('key', config.deploykey)
-    req.attach('zipfile', './widgetpack.zip');
+    req.attach('zipfile', './SingularWidget.zip');
     req.end(function(err, res) {
       if(err) {
         console.error(err);
       } else {
-        console.log('Deployed success with widget id: ' + res.body);
+        console.log('Widget ID: ' + res.body + ' successfully deployed');
       }
 
       // Cleanup
-      fs.unlink('./widgetpack.zip');
+      fs.unlink('./SingularWidget.zip');
     });
   }
 });
